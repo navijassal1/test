6d33c2ec6869d0bceacbb0d3dcdc320c1eb02db0
Navdeep Singh <navdeepjassal40@gmail.com>
2025-12-23 12:19:55 +0530

    adding stash 4

---
+++ stash4.js
@@ -0,0 +1,16 @@
+let a = 10
+let b = 20
+
+for (i = 0; i < a; i++) {
+    console.log(i)
+}
+
+if (a == b) {
+    console.log(a, 'a and b is equals...')
+}
+else if (a > b) {
+    console.log(a, 'is greater')
+}
+else {
+    console.log(b, 'is greater')
+}
